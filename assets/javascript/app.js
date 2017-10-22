59aeb755ac43eca37bdf259745d4c1361ba6f19f
LJ Reader <lawrencereader@outlook.com>
2017-10-21 23:03:15 -0600

    Charlie

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
-$('#start').on('click,function(){
+$('#start').on('click',function(){
     $('#start').remove();
-    game.loadQuestion();
-})
+    game.loadQuestion();})
 
 $(document).on('click','.answer-button',function(e){
     game.clicked(e);
